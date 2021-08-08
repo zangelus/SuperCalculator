e2b8498c1aed2b9e12417242ef12fdf401aac2f1
Haitham Abdel-Salam <hasalam@bkom.com>
2021-08-08 16:31:35 -0400

    Adding new utility classes to the main branch

--- SuperCalculator/src/supercalculator/Math.java
+++ SuperCalculator/src/supercalculator/Math.java
@@ -7,18 +7,111 @@ package supercalculator;
 
 /**
  *
- * @author za
+ * @author za, Haitham Abdel-Salam
  */
-public class Math {
-    
-    public static double factorial(int n){
-        
-        if(n == 0 || n == 1){
+public final class Math {
+
+    private Math() {
+
+    }
+    /**
+     *@PI Fixed PI value
+     */
+    public static final double PI = 3.141592653589793;
+
+    /**
+     *@PI_DEG Fixed PI value in degree
+     */
+    public static final double PI_DEG = 180;
+
+    /**
+     * Function calculates factorial of a givien integer
+     *@param n integer to calculate its factorial
+     */
+    public static double calculateFactorial(final int n) {
+        if (n == 0 || n == 1) {
             return 1.0d;
         }
-        
-        double result = n * factorial(n-1);
+
+        double result = n * calculateFactorial(n - 1);
         return result;
     }
-    
+
+    /**
+     * Converts Radian to Degree
+     *@param degreeValue value in degrees
+     */
+    public static double convertDegToRad(final double degreeValue) {
+        return degreeValue * (PI / PI_DEG);
+    }
+
+    /**
+     * Calculates power of a number
+     *@param value  number
+     * @param n power
+     */
+    public static double calculatePower(final double value, final double n) {
+        double power = 1;
+
+        for (int i = 0; i < n; i++) {
+            power = power * value;
+        }
+
+        return power;
+    }
+
+    /**
+     * Validates if a number is numeric
+     *@param string  the numeric value in string format to be checked on
+     */
+    public static boolean isValueNumericDouble(final String string) {
+        double value;
+
+        if (string == null || string.equals("")) {
+            return false;
+        }
+
+        try {
+            value = Double.parseDouble(string);
+            return true;
+        } catch (NumberFormatException e) {
+            System.out.println(" your input can't be parsed to a number");
+        }
+        return false;
+    }
+
+    /**
+     * Validates if a number is numeric
+     *@param string  the numeric value in string format to be checked on
+     */
+    public static boolean isValueNumericInteger(final String string) {
+        double value;
+
+        if (string == null || string.equals("")) {
+            return false;
+        }
+
+        try {
+            value = Integer.parseInt(string);
+            return true;
+        } catch (NumberFormatException e) {
+            System.out.println(" your input can't be parsed to a number");
+        }
+        return false;
+    }
+
+    /**
+     * Validates if a number is numeric
+     *@param num  the value to be rounded
+     * @param  places decimal places
+     */
+    public static double roundToDecimalPlaces(double num, int places) {
+        if (places < 0) throw new IllegalArgumentException();
+
+        String formattedNumber = String.format("%."+places+"f", num);
+
+        return Double.parseDouble(formattedNumber);
+    }
 }
+
+
