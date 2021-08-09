a5676cffab2feff8ad595c34914f3fc306f03e40
Leandro Rodrigues <lbrguitar@gmail.com>
2021-08-09 15:50:23 -0400

    Implementation of the method calculate

--- SuperCalculator/src/supercalculator/Power.java
+++ SuperCalculator/src/supercalculator/Power.java
@@ -20,11 +20,22 @@ public class Power {
     public Power() { }
 
     /**
-     * Calculate the result of the base elevated to the given power
+     * Calculates the result of the base elevated to the given power using the binary exponentiation algorithm
      * @param base The number that is the base for the operation
      * @param power The number to which the base is elevated to
      */
     public double calculate(double base, double power) {
+        double result = 1; // Holds the result over the steps
+        double b = base; // Keeps control of base multiplications
 
+        while (power > 0) {
+            if(power & 1 == 1)
+                result = result * base;
+
+            b = b * b;
+            power >>= 1;
+        }
+
+        return result;
     }
 }
