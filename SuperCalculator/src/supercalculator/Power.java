013d038623a9e49f4816c8b5c4838e1a1afe2da0
Leandro Rodrigues <lbrguitar@gmail.com>
2021-08-09 15:26:16 -0400

    Created Power function class

---
+++ SuperCalculator/src/supercalculator/Power.java
@@ -0,0 +1,30 @@
+/*
+ * Power class
+ *
+ * August 9th 2021
+ *
+ */
+
+package supercalculator;
+
+import java.util.Scanner;
+
+/**
+ * Power function for super calculator SOEN 6011
+ */
+public class Power {
+
+    /**
+    * Default constructor
+    */
+    public Power() { }
+
+    /**
+     * Calculate the result of the base elevated to the given power
+     * @param base The number that is the base for the operation
+     * @param power The number to which the base is elevated to
+     */
+    public double calculate(double base, double power) {
+
+    }
+}
