5f8979e9fde27d1bf6fbac3a13cd799eb7beb6c2
Leandro Rodrigues <lbrguitar@gmail.com>
2021-08-09 19:19:20 -0400

    Added condition for exponent being 0

--- SuperCalculator/src/supercalculator/Power.java
+++ SuperCalculator/src/supercalculator/Power.java
@@ -25,8 +25,14 @@ public class Power {
      * @param power The number to which the base is elevated to
      */
     public double calculate(double base, double power) {
+        /* TODO: Must add condition for if the base is zero (handle in input)*/
+        /* TODO: Also must add unit tests */
         double result = 1; // Holds the result over the steps
         double b = base; // Keeps control of base multiplications
+
+        /* Any number to the power of 0 is 1 */
+        if(power == 0)
+            return 1;
 
         while (power > 0) {
             if(power & 1 == 1)
@@ -37,5 +43,16 @@ public class Power {
         }
 
         return result;
+    }
+
+    /**
+     * Handles user input and output
+     */
+    public void getInput() {
+        Scanner scanner = new Scanner(System.in);
+        double inputBase = 0;
+        double inputPower = 0;
+
+        
     }
 }
