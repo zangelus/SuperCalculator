3b9ce8aaeccb7fc8b8150f7579c4cadf39da0d5a
Diriks <34445283+Diriks@users.noreply.github.com>
2021-08-11 09:19:32 -0400

    Update CalculatorUtils.java

--- SuperCalculator/src/supercalculator/CalculatorUtils.java
+++ SuperCalculator/src/supercalculator/CalculatorUtils.java
@@ -47,5 +47,44 @@ public class CalculatorUtils {
         System.out.println();
         
     }
+    
+    public  static double getGammaOutput(double result)
+    {
+        while(true) {
+
+            System.out.println("Please enter the number of decimal places. (n) for No. Value must be > 0");
+            Scanner scannerInput = new Scanner(System.in);
+            String choice = scannerInput.nextLine();
+
+            if (choice.equalsIgnoreCase("n")) {
+                break;
+            }
+            else
+            {
+                try {
+                    if (Integer.parseInt(choice) < 0  || choice.matches("^\\d+\\.\\d+") ) {
+                        System.err.println(" Please enter an Integer bigger than 0");
+                        continue;
+                    }
+                }catch(NumberFormatException ex){
+                    System.err.println(" Please enter an Integer bigger than 0");
+                    continue;
+                }
+
+                result = Math.roundToDecimalPlaces(result, Integer.parseInt(choice));
+                break;
+            }
+        }
+        return result;
+    }
+
+    public  static void printGamma(String message){
+
+        System.out.println("___________________________________");
+        System.out.println(" You answer is " + message);
+        System.out.println("___________________________________");
+        System.out.println();
+
+    }
 
 }
